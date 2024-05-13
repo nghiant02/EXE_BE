39b77312db8a7b90eab7e12f5ff72c379cc8c7bc
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-05-13 12:38:13 +0700

    modify

--- EXE201.BLL/Interfaces/IUserServices.cs
+++ EXE201.BLL/Interfaces/IUserServices.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EXE201.DAL.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace EXE201.BLL.Interfaces
 {
-    public class IUserServices
+    public interface IUserServices
     {
     }
 }
