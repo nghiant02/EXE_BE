38de9f98ecf8462aa95777d172ab81eb9cdadeeb
thangnguyen8520 <130530198+thangnguyen8520@users.noreply.github.com>
2024-05-16 18:14:07 +0700

    AddProduct and GetAllProduct

--- EXE201.BLL/Interfaces/IProductServices.cs
+++ EXE201.BLL/Interfaces/IProductServices.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EXE201.BLL.DTOs.UserDTOs;
+using EXE201.DAL.Models;
+using LMSystem.Repository.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,8 @@ namespace EXE201.BLL.Interfaces
 {
     public interface IProductServices
     {
+        Task<ResponeModel> AddProduct(AddProductDTO addProduct);
+        Task<IEnumerable<Product>> GetAll();
+
     }
 }
