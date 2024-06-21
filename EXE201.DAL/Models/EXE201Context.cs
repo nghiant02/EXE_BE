5362594fa13056b6aadef6bcac6e02b5e2370ff4
Nguyễn Trọng Nghĩa <nghia2852002@gmail.com>
2024-06-21 09:00:53 +0700

    scaffold

--- EXE201.DAL/Models/EXE201Context.cs
+++ EXE201.DAL/Models/EXE201Context.cs
@@ -75,9 +75,9 @@ public partial class EXE201Context : DbContext
 
     public virtual DbSet<VerifyCode> VerifyCodes { get; set; }
 
-//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-//        => optionsBuilder.UseSqlServer("Data Source=LEO\\SQLEXPRESS;Initial Catalog=EXE201;User ID=sa;Password=Abcd1234");
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=LEO\\SQLEXPRESS;Initial Catalog=EXE201;User ID=sa;Password=Abcd1234");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
