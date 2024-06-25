3dd430ee9654863451cdf1031e4e2a83af26ca24
minhnd <lammjnhphong4560@gmail.com>
2024-06-25 07:53:56 +0700

    update getProductById

--- EXE201.DAL/DTOs/ProductDTOs/ProductDetailDTO.cs
+++ EXE201.DAL/DTOs/ProductDTOs/ProductDetailDTO.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EXE201.DAL.Models;
 
 namespace EXE201.DAL.DTOs.ProductDTOs
 {
@@ -13,9 +14,13 @@ namespace EXE201.DAL.DTOs.ProductDTOs
         public string ProductName { get; set; }
         public string ProductTitle { get; set; }
         public string ProductDescription { get; set; }
+        public IEnumerable<int> ProductImageId { get; set; }
         public IEnumerable<string> ProductImage { get; set; }
         public double? ProductPrice { get; set; }
+        public IEnumerable<int> ProductSizeId { get; set; }
         public IEnumerable<string> ProductSize { get; set; }
+
+        public IEnumerable<int> ProductColorId { get; set; }
         public IEnumerable<string> ProductColor { get; set; }
         public IEnumerable<string> ProductColorImage { get; set; }
         public string ProductStatus { get; set; }
